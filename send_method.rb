1cacf36853a3dab71ea454412cd4b8a8591bb213
suman <tiwarisuman80@gmail.com>
2021-05-31 12:38:32 +0545

    refactor

--- send_method.rb
+++ send_method.rb
@@ -20,5 +20,5 @@ c1 = MyClass.new
 c1.send(:greeting)
 c1.send(:greeting_with_message, "Welcome home suman")
 
-#both private and public methods can be called by send method
+#both private and public methods can bWhae called by send method
 c1.send(:private_greeting, "This is private method greeting you")
